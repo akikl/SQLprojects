3dcad44e94faa194fa22a119d9da5b71cf5bdb86
akikl <57595385+akikl@users.noreply.github.com>
2023-06-28 19:38:40 +0530

    Delete Q5.sql

--- challenge4/Q5.sql
+++
@@ -1,9 +0,0 @@
-/*5.Get the products that have the highest and lowest manufacturing costs. The final output should contain these fields,
-product_code product manufacturing_cost*/
-
-select f.product_code,product,manufacturing_cost
-from fact_manufacturing_cost f
-join dim_product p
-on f.product_code=p.product_code
-where f.manufacturing_cost in((select max(manufacturing_cost) from fact_manufacturing_cost),
-(select min(manufacturing_cost) from fact_manufacturing_cost))
